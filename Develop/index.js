8f2ba2a9c1ec0c2535c46fd2c71a5401298bbca3
Mario Repas <mario.repas63@gmail.com>
2023-02-08 20:05:38 -0500

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,65 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer.prompt([
+  {
+    type: "input",
+    message: "What is the title of your project?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Please provide a description of your project.",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "Please provide installation instructions.",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "Please provide usage information.",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "Please provide contribution guidelines.",
+    name: "contribution",
+  },
+  {
+    type: "input",
+    message: "Please provide test instructions.",
+    name: "test",
+  },
+  {
+    type: "list",
+    message: "Please choose a license.",
+    name: "license",
+    choices: ["MIT", "Apache", "GPL", "BSD", "None"],
+  },
+  {
+    type: "input",
+    message: "Please provide your GitHub username.",
+    name: "github",
+  },
+  {
+    type: "input",
+    message: "Please provide your email address.",
+    name: "email",
+  },
+]);
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+.then((response) => {
+    fs.writeFile('README.md', `${response.title} ${response.description} ${response.installation} ${response.usage} ${response.contribution} ${response.test} ${response.license} ${response.github} ${response.email}`, (err) =>
+        err ? console.error(err) : console.log('Success!')
+    );
+});
+
+
 
 // TODO: Create a function to initialize app
 function init() {}
